294e3836773e2d3769a40af7f91ccd94d90f4b26
Kalgudin <kalgudin@mail.ru>
2023-06-30 19:46:17 +0300

    make 2 tests

--- base/simple/tests.py
+++ base/simple/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestSimple(TestCase):
+    def test_main(self):
+        response = self.client.get('/')  # получаем ответ браузера
+        self.assertEquals(response.status_code, 200)  # простая проверка кода ответа
+
+    def test_main_contains(self):
+        response = self.client.get('/')  # получаем ответ браузера
+        self.assertIn('My message', response.content.decode())  #  получаем контент в байтах и декодируем в строку
